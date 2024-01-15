e65be93a21ed1a68e8b8e279f6b0173e65ae7cc5
Pedro Luis C' Lopes <pedroluiscostalopes@hotmail.com>
2024-01-14 23:39:01 -0300

    Realizado em: 14/01/2024

---
+++ Exercício (1)/index.js
@@ -0,0 +1,10 @@
+// Variavel do input do user
+const input = 10;
+// Variavel para a soma
+let soma = 0;
+// Sistema de loop
+for (let i = 0; i <= input;i++) {
+    soma += i;
+}
+// Imprime no console
+console.log(`O valor da soma do input "${input} resulta em "${soma}."`);
